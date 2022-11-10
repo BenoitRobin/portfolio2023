34790154368703bc28e6a99e527d6d4d6d757120
BenoitRobin <benoitrobin@me.com>
2022-11-10 15:43:04 +0100

    test: :bug: bug with git

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,1 @@
-<h2>Welcome to SvelteKit</h2>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<h1 class="text-3xl font-bold underline">Hello world buddy!</h1>
+<h1 class="text-3xl font-bold">First step of my Portfolio 22'</h1>
